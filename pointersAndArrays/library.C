a118df7c947aa82222593c29afd704d55bccdcd3
fred achi <fred.achi@turntabl.io>
2021-01-28 17:55:46 +0000

    implemented strcpy function

--- pointersAndArrays/library.C
+++ pointersAndArrays/library.C
@@ -3,33 +3,59 @@
 #include <string>
 using namespace std;
 
-int myStrcmp(const char *lhs, const char *rhs){
-    int i =0;
-    while (i++, lhs[i]==rhs[i] && lhs[i] && rhs[i]);
-        return lhs[i]-rhs[i];
+// int myStrcmp(const char *lhs, const char *rhs){
+//     int i =0;
+//     while (i++, lhs[i]==rhs[i] && lhs[i] && rhs[i]);
+//         return lhs[i]-rhs[i];
         
     
     
+// }
+
+// const char myStrlen(const char* firstChar){
+
+//     //const char* lastChar = firstChar;
+//     int value=0;
+//     while(*firstChar++ !='\0'){
+//         value +=1;
+//     }
+//     //while (*lastChar++ != 0);
+//     return value;  
+// }
+
+char *mystrcat(char *dest, char *src){
+    char *copyDest = dest;
+    while(*dest)
+        dest++;
+    while(*dest++ = *src++);
+    return copyDest;
 }
 
-const char myStrlen(const char* firstChar){
-
-    //const char* lastChar = firstChar;
-    int value=0;
-    while(firstChar++ !=0){
-        value +=1;
+// char *mystrcpy( char *dest, const char *src){
+//     char *copyDest = dest;
+//    //copy elements in src into dest and return when src is empty
+//    while(*copyDest++ == *src++);//fails if length of dest is smaller than src
+//    return dest;
+// }
+char *mystrcpy(char *copy, const char *src){
+    int i, len = strlen(src);
+    for (i = 0; i < len; i++)
+    {
+        copy[i] = src[i];
     }
-    //while (*lastChar++ != 0);
-    return value;  
+    copy[i]='\0';
+    return copy;
+    
 }
 
 int main(){
 
 //cout<<" enter two words\n";
-const char firstWord[] = "you";
+char firstWord[10];
+char secondWord[]{"mehs"};
 //cin>>firstWord;
 
 //cout<<myStrcmp(firstWord.c_str(),secondWord.c_str());
-cout<<myStrlen(firstWord);
+cout<<mystrcpy(firstWord,secondWord);
 
 }
